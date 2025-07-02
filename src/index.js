cfab70b47fc49af5a3b88f1731e5481a441d543c
Subhajeetgit <subhajeetswtu34@gmail.com>
2025-07-03 00:06:01 +0530

    Utils and Error handling set

--- src/index.js
+++ src/index.js
@@ -7,6 +7,16 @@ dotenv.config({
 
 
 connectDb()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server running in port : ${process.env.PORT}`);
+        
+    })
+})
+.catch((err)=>{
+    console.log("MONGO CONNECTION FAILED !!! ", err);
+    
+})
 
 
 
